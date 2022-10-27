f098a5293f837d8e6b7c1db8a47d5b0e5e90244b
Akuya Ekorot <akuyaekorot@gmail.com>
2022-10-27 17:51:52 +0300

    build html structure for main section and social section

--- stage-1/pages/index.tsx
+++ stage-1/pages/index.tsx
@@ -2,13 +2,45 @@ import Image from "next/image";
 
 const Home = () => (
   <div className="flex flex-col">
-    <main className="flex flex-col grow"></main>
+    <section className="flex flex-col grow px-[112px] py-[64px]">
+      <main className="py-[32px]">
+        <div className="content">
+          <div className="profile-section">
+            <img />
+            <div className="twitter">
+              <p className="annette-black"></p>
+            </div>
+            <div className="avatar-share-button"></div>
+          </div>
+          <div className="link-section">
+            {Array(6).fill(0).map((link, index) => {
+              return (<div key={index}>
+                <div></div>
+              </div>);
+            })}
+          </div>
+        </div>
+      </main>
+      <div></div>
+    </section>
     <footer className="px-[48px] py-[80px]">
       <div className="px-[32px]">
         <nav className="flex flex-row gap-[32px] pt-[36px] border-t border-[#EAECF0] ">
-          <Image src="/Zuri.Internship_Logo.svg" width={`185`} height={`32`} alt={`Zuri Internship Logo`}/>
-          <p className="grow flex items-center justify-center">HNG Internship 9 Frontend Task</p>
-          <Image src="/I4G.png" width={`132`} height={`32`} alt={`Zuri Internship Logo`}/>
+          <Image
+            src="/Zuri.Internship_Logo.svg"
+            width={`185`}
+            height={`32`}
+            alt={`Zuri Internship Logo`}
+          />
+          <p className="grow flex items-center justify-center">
+            HNG Internship 9 Frontend Task
+          </p>
+          <Image
+            src="/I4G.png"
+            width={`132`}
+            height={`32`}
+            alt={`Zuri Internship Logo`}
+          />
         </nav>
       </div>
     </footer>
