5759e6b626ab628cfeea165b9cb7979424ce1d66
Akuya Ekorot <akuyaekorot@gmail.com>
2022-10-31 18:18:35 +0300

    use useState and useEffect to find the window width

--- stage-1/components/ProfileSection.tsx
+++ stage-1/components/ProfileSection.tsx
@@ -4,20 +4,21 @@ import MoreIcon from "./MoreIcon";
 import ProfilePic from "./ProfilePic";
 import ShareIcon from "./ShareIcon";
 
-const useMediaQuery = (width: number): boolean => {
-  const [targetReached, setTargetReached] = useState(false);
+const ProfileSection = ({ ...myData }: personalData) => {
+  const [deviceWidth, setDeviceWidth] = useState(768);
+
+  const handleResize = () => {
+    setDeviceWidth(window.innerWidth);
+  };
 
   useEffect(() => {
-    window.innerWidth > width
-      ? setTargetReached(true)
-      : setTargetReached(false);
-  }, []);
+    setDeviceWidth(window.innerWidth);
 
-  return targetReached;
-};
-
-const ProfileSection = ({ ...myData }: personalData) => {
-  const isBreakPoint = useMediaQuery(768);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [deviceWidth]);
 
   return (
     <div className="profile-section flex flex-col items-center gap-[24px] relative">
@@ -37,7 +38,7 @@ const ProfileSection = ({ ...myData }: personalData) => {
         href="#"
         className="peer stroke-gray-400 hover:stroke-gray-500 hover:bg-gray-50 focus:outline-none focus:ring-[4px] focus:ring-gray-100 absolute -top-[20px] right-[0px] lg:right-[208px] h-[40px] w-[40px] rounded-full border flex items-center justify-center border-dashed border-gray-300 text-gray-400"
       >
-        {isBreakPoint ? <ShareIcon /> : <MoreIcon />}
+        {deviceWidth >= 768 ? <ShareIcon /> : <MoreIcon />}
       </a>
       <div className="invisible peer-hover:visible flex items-center absolute -top-[18px] right-[38px] lg:right-[246px]">
         <div className="h-[34px] w-[85px] bg-gray-900 text-white rounded-[8px] flex items-center justify-center py-[8px] px-[12px]">
