a8036590ce351fe626a23db4501411a8aec862d7
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-01 00:45:37 +0300

    add types and subtext content in data and types files

--- stage-1/utils/pageData.ts
+++ stage-1/utils/pageData.ts
@@ -1,23 +1,46 @@
 import { pageLink } from "./types";
 
 const pageLinks: pageLink[] = [
-  { name: "Twitter Link", link: "https://twitter.com/akuya_ekorot", id: "" },
-  { name: "Zuri Team", link: "https://training.zuri.team", id: "btn__zuri" },
-  { name: "Zuri Books", link: "https://books.zuri.team", id: "books" },
   {
-    name: "Python Books",
+    name: "Follow me on Twitter",
+    link: "https://twitter.com/akuya_ekorot",
+    id: "",
+    subtext: "To lead you must learn to follow. Are you ready to lead? 😅",
+  },
+  {
+    name: "Why the Zuri Team",
+    link: "https://training.zuri.team",
+    id: "btn__zuri",
+    subtext:
+      "Breaking into tech can be hard. *Uzuri you're here 😉. Uzuri is Swahili for 'good thing'.",
+  },
+  {
+    name: "Get our Zuri Books",
+    link: "https://books.zuri.team",
+    id: "books",
+    subtext:
+      "Design and coding advice is all over the place. The best ones are distilled in our books.",
+  },
+  {
+    name: "Get Started With Python",
     link: "https://books.zuri.team/python-for-beginners?ref_id=akuya-ekorot",
     id: "book__python",
+    subtext:
+      "If I was facing (a) python 🐍, I would want to know what to do. This book could save your life!",
   },
   {
     name: "Background Check for Coders",
     link: "https://background.zuri.team",
     id: "pitch",
+    subtext:
+      "Don't know what to look for in a great coder? Let us run a background check for you ... or let them run away with weeks worth of work not delivered.",
   },
   {
     name: "Design Books",
     link: "https://books.zuri.team/design-rules",
     id: "book__design",
+    subtext:
+      "Great design get's out of the way and let's the user focus on what's important. The same way I'm getting out of the way so that you can click this link. 😅",
   },
 ];
 
