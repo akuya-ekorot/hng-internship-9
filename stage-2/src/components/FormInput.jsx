b375f8eaa49e1e32b69b2a930ece050d34abe7f8
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-03 19:33:44 +0300

    feat: add input fields to form in contact page

---
+++ stage-2/src/components/FormInput.jsx
@@ -0,0 +1,23 @@
+const FormInput = ({ label, placeholder, type }) => {
+  const tailwindStyles =
+    "border border-gray-300 focus:border-primary-300 rounded-lg py-[10px] px-[14px] text-md text-gray-900 placeholder:text-md placeholder:text-gray-500 focus:outline-none";
+  return (
+    <div className="flex flex-col gap-[6px] grow">
+      <label className="text-gray-700 text-sm font-medium">{label}</label>
+      {type != "textarea" ? (
+        <input
+          className={tailwindStyles}
+          type={type}
+          placeholder={placeholder}
+        />
+      ) : (
+        <textarea
+          className={`${tailwindStyles} h-[130px]`}
+          placeholder={placeholder}
+        ></textarea>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
--- stage-2/src/routes/contact.jsx
+++ stage-2/src/routes/contact.jsx
@@ -1,7 +1,47 @@
+import Footer from "../components/Footer";
+import { Form } from "react-router-dom";
+import FormInput from "../components/FormInput";
+
 const Contact = () => {
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
-      <h1 className="text-display-2xl font-bold">Contact Me</h1>
+    <div className="w-full flex flex-col items-stretch pt-[64px] pb-[32px] md:pb-[64px] gap-[24px] md:px-[112px] px-[16px]">
+      <section className="w-full flex items-stretch max-w-[1216px] m-auto">
+        <main className="md:px-[32px] grow">
+          <div className="content flex flex-col md:gap-[56px] gap-[32px]">
+            <header className="flex flex-col gap-[20px]">
+              <h1 className="text-display-md font-semibold">Contact Me</h1>
+              <p className="text-xl font-normal">
+                Hi there, contact me to ask me about anything you have in mind.
+              </p>
+            </header>
+            <Form className="flex flex-col gap-[24px]">
+              <div className="flex gap-[24px]">
+                <FormInput
+                  label={`First Name`}
+                  placeholder={`Enter your first name`}
+                  type={`text`}
+                />
+                <FormInput
+                  label={`Last Name`}
+                  placeholder={`Enter your last name`}
+                  type={`text`}
+                />
+              </div>
+              <FormInput
+                label={`Email`}
+                placeholder={`yourname@email.com`}
+                type={`email`}
+              />
+              <FormInput
+                label={`Message`}
+                placeholder={`Send me a message and I'll reply as soon as possible...`}
+                type={`textarea`}
+              />
+            </Form>
+          </div>
+        </main>
+      </section>
+      <Footer />
     </div>
   );
 };
