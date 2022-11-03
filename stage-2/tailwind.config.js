864f80479b600fb90c46898a952061bdb40c42ee
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-03 14:25:37 +0300

    chore: configure typography styles in tailwindcss

--- stage-2/tailwind.config.js
+++ stage-2/tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "display-2xl": [
+          "4.5rem",
+          {
+            lineHeight: "5.625rem",
+            tracking: "-2%",
+          },
+        ],
+        "display-xl": [
+          "3.75rem",
+          {
+            lineHeight: "4.5rem",
+            tracking: "-2%",
+          },
+        ],
+        "display-lg": [
+          "3rem",
+          {
+            lineHeight: "3.75rem",
+            tracking: "-2%",
+          },
+        ],
+        "display-md": [
+          "2.25rem",
+          {
+            lineHeight: "2.75rem",
+            tracking: "-2%",
+          },
+        ],
+        "display-sm": ["1.875rem", "2.375rem"],
+        "display-xs": ["1.5rem", "2rem"],
+        xl: ["1.25rem", "1.875rem"],
+        lg: ["1.125rem", "1.75rem"],
+        md: ["1rem", "1.5rem"],
+        sm: ["0.875rem", "1.25rem"],
+        xs: ["0.75rem", "1.125rem"],
+      },
+    },
   },
   plugins: [],
 };
