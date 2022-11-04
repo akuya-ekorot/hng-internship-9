3dee45c8b9e6cdd67b0f497c4c7102853bcc921a
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-04 17:19:06 +0300

    style: fix error and focus states on form input

--- stage-2/src/components/FormInput.jsx
+++ stage-2/src/components/FormInput.jsx
@@ -1,21 +1,36 @@
+import { useEffect, useRef } from "react";
+import { useState } from "react";
+
 const FormInput = ({ label, placeholder, type, tabIndex }) => {
+  const [initialFocus, setInitialFocus] = useState(false);
+
   const tailwindStyles =
-    "border border-gray-300 focus:border-primary-300 rounded-lg py-[10px] px-[14px] md:text-md text-sm text-gray-900 placeholder:text-md placeholder:text-gray-500 focus:outline-none";
+    "border placeholder-shown:border-gray-300 focus:border-primary-300 rounded-lg py-[10px] px-[14px] md:text-md text-sm text-gray-900 placeholder:text-md placeholder:text-gray-500 focus:outline-none";
   return (
     <div className="flex flex-col gap-[6px] grow">
       <label className="text-gray-700 text-sm font-medium">{label}</label>
       {type !== "textarea" ? (
         <input
-          className={`${tailwindStyles} invalid:border-error-default invalid:focus:ring-error-focused`}
+          className={`${tailwindStyles} ${
+            initialFocus &&
+            "invalid:border-error-default invalid:focus:ring-error-focused"
+          }`}
           type={type}
           placeholder={placeholder}
           tabIndex={tabIndex}
+          onBlur={() => setInitialFocus(true)}
+          required
         />
       ) : (
         <textarea
-          className={`${tailwindStyles} h-[130px] focus:border-primary-300 focus:ring focus:ring-primary-100 invalid:border-error-default invalid:focus:ring-error-focused `}
+          className={`${tailwindStyles} h-[130px] focus:border-primary-300 focus:ring focus:ring-primary-100 ${
+            initialFocus &&
+            "invalid:border-error-default invalid:focus:ring-error-focused invalid:focus:border-error-default"
+          }`}
           placeholder={placeholder}
           tabIndex={tabIndex}
+          onBlur={() => setInitialFocus(true)}
+          required
         ></textarea>
       )}
     </div>
